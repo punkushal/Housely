43c34aa080121db8e391e2ea243da8f8fd9325e7
punkushal <spycymagar@gmail.com>
2025-12-15 16:55:31 +0545

    chore: configure firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:housely/core/constants/app_text_style.dart';
+import 'package:housely/core/theme/app_theme.dart';
+import 'package:housely/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize firebase app
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -10,6 +18,33 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Housely');
+    return MaterialApp(
+      title: 'Housely',
+      theme: AppTheme.light(context),
+      home: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              mainAxisAlignment: .center,
+              children: [
+                Text(
+                  'Heading Large',
+                  style: AppTextStyle.headingRegular(context),
+                ),
+                Text('Body Regualar', style: AppTextStyle.bodyRegular(context)),
+                Text(
+                  'label semi bold',
+                  style: AppTextStyle.labelSemiBold(context),
+                ),
+                TextField(decoration: InputDecoration(hintText: 'UserName')),
+                Checkbox(value: false, onChanged: (value) {}),
+                ElevatedButton(onPressed: () {}, child: Text("Button")),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
