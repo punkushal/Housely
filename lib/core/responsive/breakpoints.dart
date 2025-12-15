2db1ce205e6ff58a122297927640a1ccdcc3b7c3
punkushal <spycymagar@gmail.com>
2025-12-15 11:35:33 +0545

    feat(theme): add breakpoints for font size

---
+++ lib/core/responsive/breakpoints.dart
@@ -0,0 +1,8 @@
+class Breakpoints {
+  Breakpoints._();
+
+  /// Break points for font size on different mobile devices width
+  static double smallPhone = 360;
+  static double normalPhone = 390;
+  static double largePhone = 412;
+}
