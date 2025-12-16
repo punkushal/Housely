4c4fcf7e436162489a92b9f7b3e9784f922217e6
punkushal <spycymagar@gmail.com>
2025-12-16 15:07:45 +0545

    chore(assets): add only 3 required image assets for onboarding

--- lib/core/constants/image_constant.dart
+++ lib/core/constants/image_constant.dart
@@ -15,10 +15,4 @@ class ImageConstant {
       "${_baseImageUrl}onboarding/onboarding_img2.png";
   static final String thirdOnBoardingImg =
       "${_baseImageUrl}onboarding/onboarding_img3.png";
-  static final String fourthOnBoardingImg =
-      "${_baseImageUrl}onboarding/onboarding_img4.png";
-  static final String fivthOnBoardingImg =
-      "${_baseImageUrl}onboarding/onboarding_img5.png";
-  static final String sixthOnBoardingImg =
-      "${_baseImageUrl}onboarding/onboarding_img6.png";
 }
