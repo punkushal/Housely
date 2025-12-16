ccd54908f28e208d6292a8d4449e8ec3c323369a
punkushal <spycymagar@gmail.com>
2025-12-16 10:55:38 +0545

    feat(constants): add logo icon and onboarding images

---
+++ lib/core/constants/image_constant.dart
@@ -0,0 +1,24 @@
+class ImageConstant {
+  ImageConstant._();
+
+  // base assets url
+  static const String _baseIconUrl = "assets/icons/";
+  static const String _baseImageUrl = "assets/images/";
+
+  // icons
+  static final String logoIcon = "${_baseIconUrl}logo_icon.png";
+
+  // onboarding images
+  static final String firstOnBoardingImg =
+      "${_baseImageUrl}onboarding/onboarding_img1";
+  static final String secondOnBoardingImg =
+      "${_baseImageUrl}onboarding/onboarding_img2";
+  static final String thirdOnBoardingImg =
+      "${_baseImageUrl}onboarding/onboarding_img3";
+  static final String fourthOnBoardingImg =
+      "${_baseImageUrl}onboarding/onboarding_img4";
+  static final String fivthOnBoardingImg =
+      "${_baseImageUrl}onboarding/onboarding_img5";
+  static final String sixthOnBoardingImg =
+      "${_baseImageUrl}onboarding/onboarding_img6";
+}
