ab41819796d9dd1aa53407324c37c2d89bd1cee1
punkushal <spycymagar@gmail.com>
2025-12-17 13:43:36 +0545

    update(onboarding): add required parameter to setOnboardingStatus method

--- lib/features/onboarding/domain/repositories/onboarding_repository.dart
+++ lib/features/onboarding/domain/repositories/onboarding_repository.dart
@@ -2,7 +2,7 @@ import 'package:housely/core/utils/typedef.dart';
 
 abstract interface class OnboardingRepository {
   /// Set Onboarding status
-  ResultVoid setOnboardingStatus();
+  ResultVoid setOnboardingStatus({required bool isFirstTime});
 
   /// Get Onboarding status
   ResultFuture<bool> getOnboardingStatus();
