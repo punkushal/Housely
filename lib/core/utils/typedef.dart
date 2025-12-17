20e441cc625f09f323e29ce990cb435a65fef28e
punkushal <spycymagar@gmail.com>
2025-12-17 14:25:42 +0545

    feat(core): add Future keyword to ResultFuture type

--- lib/core/utils/typedef.dart
+++ lib/core/utils/typedef.dart
@@ -1,5 +1,5 @@
 import 'package:fpdart/fpdart.dart';
 import 'package:housely/core/error/failure.dart';
 
-typedef ResultFuture<T> = Either<Failure, T>;
+typedef ResultFuture<T> = Future<Either<Failure, T>>;
 typedef ResultVoid = ResultFuture<void>;
