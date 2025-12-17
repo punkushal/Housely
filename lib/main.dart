b014ec93e028c2cd766cd6eebbbefbbb8ff8c2cd
punkushal <spycymagar@gmail.com>
2025-12-17 16:50:19 +0545

    chore(dependency injection): initialize dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,15 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:housely/app/app.dart';
 import 'package:housely/firebase_options.dart';
+import 'package:housely/injection_container.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Initialize firebase app
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Initialize dependencies
+  await initializeDependencies();
   runApp(const MyApp());
 }
