00af1ef630d58d8ea038cf6af6eb2d0ae4661152
punkushal <spycymagar@gmail.com>
2025-12-18 09:58:16 +0545

    feat(cubit): add bloc provider for onboarding cubit

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:housely/app/app_router.dart';
 import 'package:housely/core/theme/app_theme.dart';
+import 'package:housely/features/onboarding/presentation/cubit/onboarding_cubit.dart';
+import 'package:housely/injection_container.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -9,11 +12,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final appRouter = AppRouter();
-    return MaterialApp.router(
-      title: 'Housely',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.light(context),
-      routerConfig: appRouter.config(),
+    return BlocProvider(
+      create: (context) => sl<OnboardingCubit>()..checkStatus(),
+      child: MaterialApp.router(
+        title: 'Housely',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.light(context),
+        routerConfig: appRouter.config(),
+      ),
     );
   }
 }
