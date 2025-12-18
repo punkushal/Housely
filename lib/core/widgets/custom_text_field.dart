2ebaf97a0482c33b5d6ebd8b225bc447f3970161
punkushal <spycymagar@gmail.com>
2025-12-18 14:52:51 +0545

    feat(ui): add reusable custom text field widget

---
+++ lib/core/widgets/custom_text_field.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({
+    super.key,
+    this.controller,
+    this.hintText,
+    this.keyboardType,
+    this.hintStyle,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.obscureText = false,
+    this.validator,
+    this.onChanged,
+  });
+
+  /// [TextEditingController] controller
+  final TextEditingController? controller;
+
+  /// hint text
+  final String? hintText;
+
+  /// hint style
+  final TextStyle? hintStyle;
+
+  /// keyboard type
+  final TextInputType? keyboardType;
+
+  /// prefix icon
+  final Widget? prefixIcon;
+
+  /// suffix icon
+  final Widget? suffixIcon;
+
+  /// obscure the text, by default false
+  final bool obscureText;
+
+  /// validator function
+  final String? Function(String?)? validator;
+
+  /// on changed function
+  final void Function(String)? onChanged;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      keyboardType: keyboardType,
+      obscureText: obscureText,
+      validator: validator,
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: hintStyle,
+        prefixIcon: prefixIcon,
+        suffixIcon: suffixIcon,
+      ),
+    );
+  }
+}
