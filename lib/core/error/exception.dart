a952aec0a86e5994cf5752ce48b154d03e87eaa1
punkushal <spycymagar@gmail.com>
2025-12-19 11:22:00 +0545

    feat(core): add custom exceptions

---
+++ lib/core/error/exception.dart
@@ -0,0 +1,14 @@
+class ServerException implements Exception {
+  final String message;
+  ServerException([this.message = 'Server error occurred']);
+}
+
+class NetworkException implements Exception {
+  final String message;
+  NetworkException([this.message = 'No internet connection']);
+}
+
+class AuthException implements Exception {
+  final String message;
+  AuthException([this.message = 'Authentication failed']);
+}
