7877d6c871180435e1f5b9b4e87931dd67776ef6
punkushal <spycymagar@gmail.com>
2025-12-19 09:52:54 +0545

    feat(ui): add highlight text parameter and logic to display that content

--- lib/features/auth/presentation/widgets/checkbox_section.dart
+++ lib/features/auth/presentation/widgets/checkbox_section.dart
@@ -5,19 +5,24 @@ import 'package:housely/core/responsive/responsive_dimensions.dart';
 class CheckboxSection extends StatelessWidget {
   const CheckboxSection({
     super.key,
-    required this.labelText,
+    this.labelText,
     required this.value,
     required this.onChanged,
+    this.hasHighlightText = false,
   });
 
   /// check box side label text
-  final String labelText;
+  final String? labelText;
 
   /// Check box actual value
   final bool value;
 
   /// Check box on changed function
   final void Function(bool? value)? onChanged;
+
+  /// To check whether the text content has highlighted parts
+  /// by default false
+  final bool hasHighlightText;
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -28,7 +33,28 @@ class CheckboxSection extends StatelessWidget {
           height: ResponsiveDimensions.getHeight(context, 16),
           child: Checkbox(value: value, onChanged: onChanged),
         ),
-        Text(labelText, style: AppTextStyle.bodyRegular(context, fontSize: 14)),
+        hasHighlightText
+            ? RichText(
+                text: TextSpan(
+                  text: 'Agree with ',
+                  style: AppTextStyle.bodyRegular(context, fontSize: 14),
+                  children: [
+                    TextSpan(
+                      text: 'terms ',
+                      style: AppTextStyle.bodySemiBold(context),
+                    ),
+                    TextSpan(text: 'and '),
+                    TextSpan(
+                      text: 'privacy',
+                      style: AppTextStyle.bodySemiBold(context),
+                    ),
+                  ],
+                ),
+              )
+            : Text(
+                labelText!,
+                style: AppTextStyle.bodyRegular(context, fontSize: 14),
+              ),
       ],
     );
   }
