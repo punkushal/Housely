f2a0140a373c05fa5b02ce592c646751bec18513
punkushal <spycymagar@gmail.com>
2025-12-22 13:43:58 +0545

    feat(ui): add logout states

---
+++ lib/features/auth/presentation/cubit/logout_state.dart
@@ -0,0 +1,19 @@
+part of 'logout_cubit.dart';
+
+sealed class LogoutState extends Equatable {
+  const LogoutState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class LogoutInitial extends LogoutState {}
+
+final class LogoutLoading extends LogoutState {}
+
+final class LogoutSuccess extends LogoutState {}
+
+final class LogoutFailure extends LogoutState {
+  final String message;
+  const LogoutFailure(this.message);
+}
