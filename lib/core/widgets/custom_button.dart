4efb6397d58dac1045c4e2a345069aa165695158
punkushal <spycymagar@gmail.com>
2025-12-23 15:17:06 +0545

    refactor(ui): refactor custom button to implement outline as well as elevated button

--- lib/core/widgets/custom_button.dart
+++ lib/core/widgets/custom_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:housely/core/constants/app_colors.dart';
+import 'package:housely/core/constants/app_text_style.dart';
 import 'package:housely/core/responsive/responsive_dimensions.dart';
 
 class CustomButton extends StatelessWidget {
@@ -7,41 +8,79 @@ class CustomButton extends StatelessWidget {
   const CustomButton({
     super.key,
     this.height,
-    this.child,
     required this.onTap,
     this.horizontal = 0,
+    required this.buttonLabel,
+    this.isLoading = false,
+    this.textColor,
+    this.fontSize,
+    this.lineHeight,
+    this.isOutlined = false,
   });
 
   /// Button's height
   final double? height;
 
-  /// Widget
-  final Widget? child;
+  /// button label text
+  final String buttonLabel;
 
   /// Button's on tap function
-  final void Function() onTap;
+  final void Function()? onTap;
 
   /// Padding value for horizontal
   final double horizontal;
+
+  /// loading checker
+  final bool isLoading;
+
+  /// outline checker
+  final bool isOutlined;
+
+  /// button label text color
+  final Color? textColor;
+
+  /// text font size
+  final double? fontSize;
+
+  /// text line height
+  final double? lineHeight;
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        margin: ResponsiveDimensions.paddingSymmetric(
-          context,
-          horizontal: horizontal,
-        ),
-        alignment: .center,
+    return Padding(
+      padding: ResponsiveDimensions.paddingSymmetric(
+        context,
+        horizontal: horizontal,
+      ),
+      child: SizedBox(
         width: double.infinity,
-        height: height == null
-            ? ResponsiveDimensions.buttonHeight(context)
-            : ResponsiveDimensions.buttonHeight(context, buttonHeight: height),
-        decoration: BoxDecoration(
-          borderRadius: ResponsiveDimensions.borderRadiusSmall(context),
-          color: AppColors.primary,
+        child: ElevatedButton(
+          onPressed: onTap,
+          style: ElevatedButton.styleFrom(
+            backgroundColor: isOutlined
+                ? AppColors.surface
+                : AppColors.primaryPressed,
+            foregroundColor: AppColors.surface,
+            shape: RoundedRectangleBorder(
+              borderRadius: ResponsiveDimensions.borderRadiusSmall(context),
+              side: BorderSide(
+                color: isOutlined ? AppColors.primary : Colors.transparent,
+              ),
+            ),
+          ),
+          child: isLoading
+              ? CircularProgressIndicator(
+                  color: isOutlined ? AppColors.primary : AppColors.surface,
+                )
+              : Text(
+                  buttonLabel,
+                  style: AppTextStyle.bodyRegular(
+                    context,
+                    color: textColor ?? AppColors.surface,
+                    fontSize: fontSize ?? 18,
+                    lineHeight: lineHeight ?? 27,
+                  ),
+                ),
         ),
-        child: child,
       ),
     );
   }
