d19fa5e5cb169700c5474baad39f9286e443604a
punkushal <spycymagar@gmail.com>
2025-12-25 13:43:04 +0545

    chore(constant): add icon assets

--- lib/core/constants/image_constant.dart
+++ lib/core/constants/image_constant.dart
@@ -8,6 +8,45 @@ class ImageConstant {
   // icons
   static final String logoIcon = "${_baseIconUrl}logo_icon.png";
   static final String googleIcon = "${_baseIconUrl}google_icon.png";
+  static final String acIcon = "${_baseIconUrl}ac_icon.png";
+  static final String bedIcon = "${_baseIconUrl}bed_icon.png";
+  static final String calenderIcon = "${_baseIconUrl}calender_icon.png";
+  static final String cameraIcon = "${_baseIconUrl}camera_icon.png";
+  static final String chatIcon = "${_baseIconUrl}chat_icon.png";
+  static final String discoveryIcon = "${_baseIconUrl}discovery_icon.png";
+  static final String discoveryFilledIcon =
+      "${_baseIconUrl}discovery_filled_icon.png";
+  static final String documentFilledIcon =
+      "${_baseIconUrl}document_filled_icon.png";
+  static final String favoriteFilledIcon =
+      "${_baseIconUrl}favorite_filled_icon.png";
+  static final String locationFilledIcon =
+      "${_baseIconUrl}location_filled_icon.png";
+  static final String paperFilledIcon = "${_baseIconUrl}paper_filled_icon.png";
+  static final String personFilledIcon =
+      "${_baseIconUrl}person_filled_icon.png";
+  static final String favoriteIcon = "${_baseIconUrl}favorite_icon.png";
+  static final String personIcon = "${_baseIconUrl}person_icon.png";
+  static final String removeIcon = "${_baseIconUrl}remove_icon.png";
+  static final String searchIcon = "${_baseIconUrl}search_icon.png";
+  static final String settingIcon = "${_baseIconUrl}setting_icon.png";
+  static final String showIcon = "${_baseIconUrl}show_icon.png";
+  static final String starIcon = "${_baseIconUrl}star_icon.png";
+  static final String locationIcon = "${_baseIconUrl}location_icon.png";
+  static final String notificationIcon = "${_baseIconUrl}notification_icon.png";
+  static final String filterIcon = "${_baseIconUrl}filter_icon.png";
+  static final String hideIcon = "${_baseIconUrl}hide_icon.png";
+  static final String historyIcon = "${_baseIconUrl}history_icon.png";
+  static final String homeIcon = "${_baseIconUrl}home_icon.png";
+  static final String infoSquareIcon = "${_baseIconUrl}info_square_icon.png";
+  static final String timeSquareIcon = "${_baseIconUrl}time_square_icon.png";
+  static final String documentIcon = "${_baseIconUrl}document_icon.png";
+  static final String bathTubIcon = "${_baseIconUrl}bathtub_icon.png";
+  static final String wifiIcon = "${_baseIconUrl}wifi_icon.png";
+  static final String arrowLeftCircleIcon =
+      "${_baseIconUrl}arrow_left_circle_icon.png";
+  static final String arrowRightCircleIcon =
+      "${_baseIconUrl}arrow_right_circle_icon.png";
 
   // onboarding images
   static final String firstOnBoardingImg =
