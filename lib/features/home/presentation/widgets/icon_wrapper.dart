d49c7acd6c3aa15b1bf0777860ca9f11a39ae502
punkushal <spycymagar@gmail.com>
2025-12-25 16:21:16 +0545

    feat(ui): add custom icon wrapper widget

---
+++ lib/features/home/presentation/widgets/icon_wrapper.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:housely/core/constants/app_colors.dart';
+import 'package:housely/core/responsive/responsive_dimensions.dart';
+
+class IconWrapper extends StatelessWidget {
+  const IconWrapper({super.key, required this.iconPath, this.onTap});
+
+  /// icon path
+  final String iconPath;
+
+  /// on tap function
+  final void Function()? onTap;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: ResponsiveDimensions.getSize(context, 44),
+        height: ResponsiveDimensions.getHeight(context, 44),
+        decoration: BoxDecoration(
+          shape: .circle,
+          border: Border.all(color: AppColors.textHint),
+        ),
+        child: SvgPicture.asset(
+          iconPath,
+          height: ResponsiveDimensions.getHeight(context, 24),
+          width: ResponsiveDimensions.getSize(context, 24),
+        ),
+      ),
+    );
+  }
+}
