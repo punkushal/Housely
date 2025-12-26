478af6544eb0155affac3fde8df2559774ed7311
punkushal <spycymagar@gmail.com>
2025-12-26 11:36:24 +0545

    feat(routing): configure tab wrapper route

--- lib/app/app_router.gr.dart
+++ lib/app/app_router.gr.dart
@@ -14,18 +14,18 @@ import 'package:flutter/material.dart' as _i10;
 import 'package:housely/features/auth/presentation/pages/forgot_password_page.dart'
     as _i1;
 import 'package:housely/features/auth/presentation/pages/login_page.dart'
-    as _i5;
-import 'package:housely/features/auth/presentation/pages/signup_page.dart'
-    as _i7;
-import 'package:housely/features/home/presentation/pages/home_page.dart' as _i2;
-import 'package:housely/features/location/presentation/pages/location_page.dart'
-    as _i3;
-import 'package:housely/features/location/presentation/pages/map_picker_page.dart'
     as _i4;
-import 'package:housely/features/onboarding/presentation/pages/onboarding_page.dart'
+import 'package:housely/features/auth/presentation/pages/signup_page.dart'
     as _i6;
+import 'package:housely/features/home/presentation/pages/home_page.dart' as _i8;
+import 'package:housely/features/location/presentation/pages/location_page.dart'
+    as _i2;
+import 'package:housely/features/location/presentation/pages/map_picker_page.dart'
+    as _i3;
+import 'package:housely/features/onboarding/presentation/pages/onboarding_page.dart'
+    as _i5;
 import 'package:housely/features/onboarding/presentation/pages/splash_page.dart'
-    as _i8;
+    as _i7;
 
 /// generated route for
 /// [_i1.ForgotPasswordPage]
@@ -44,53 +44,7 @@ class ForgotPasswordRoute extends _i9.PageRouteInfo<void> {
 }
 
 /// generated route for
-/// [_i2.HomePage]
-class HomeRoute extends _i9.PageRouteInfo<HomeRouteArgs> {
-  HomeRoute({_i10.Key? key, String? address, List<_i9.PageRouteInfo>? children})
-    : super(
-        HomeRoute.name,
-        args: HomeRouteArgs(key: key, address: address),
-        initialChildren: children,
-      );
-
-  static const String name = 'HomeRoute';
-
-  static _i9.PageInfo page = _i9.PageInfo(
-    name,
-    builder: (data) {
-      final args = data.argsAs<HomeRouteArgs>(
-        orElse: () => const HomeRouteArgs(),
-      );
-      return _i2.HomePage(key: args.key, address: args.address);
-    },
-  );
-}
-
-class HomeRouteArgs {
-  const HomeRouteArgs({this.key, this.address});
-
-  final _i10.Key? key;
-
-  final String? address;
-
-  @override
-  String toString() {
-    return 'HomeRouteArgs{key: $key, address: $address}';
-  }
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-    if (other is! HomeRouteArgs) return false;
-    return key == other.key && address == other.address;
-  }
-
-  @override
-  int get hashCode => key.hashCode ^ address.hashCode;
-}
-
-/// generated route for
-/// [_i3.LocationPage]
+/// [_i2.LocationPage]
 class LocationRoute extends _i9.PageRouteInfo<void> {
   const LocationRoute({List<_i9.PageRouteInfo>? children})
     : super(LocationRoute.name, initialChildren: children);
@@ -100,13 +54,13 @@ class LocationRoute extends _i9.PageRouteInfo<void> {
   static _i9.PageInfo page = _i9.PageInfo(
     name,
     builder: (data) {
-      return const _i3.LocationPage();
+      return const _i2.LocationPage();
     },
   );
 }
 
 /// generated route for
-/// [_i4.LocationWrapper]
+/// [_i3.LocationWrapper]
 class LocationWrapper extends _i9.PageRouteInfo<void> {
   const LocationWrapper({List<_i9.PageRouteInfo>? children})
     : super(LocationWrapper.name, initialChildren: children);
@@ -116,13 +70,13 @@ class LocationWrapper extends _i9.PageRouteInfo<void> {
   static _i9.PageInfo page = _i9.PageInfo(
     name,
     builder: (data) {
-      return const _i4.LocationWrapper();
+      return const _i3.LocationWrapper();
     },
   );
 }
 
 /// generated route for
-/// [_i5.LoginPage]
+/// [_i4.LoginPage]
 class LoginRoute extends _i9.PageRouteInfo<void> {
   const LoginRoute({List<_i9.PageRouteInfo>? children})
     : super(LoginRoute.name, initialChildren: children);
@@ -132,13 +86,13 @@ class LoginRoute extends _i9.PageRouteInfo<void> {
   static _i9.PageInfo page = _i9.PageInfo(
     name,
     builder: (data) {
-      return const _i5.LoginPage();
+      return const _i4.LoginPage();
     },
   );
 }
 
 /// generated route for
-/// [_i6.OnboardingPage]
+/// [_i5.OnboardingPage]
 class OnboardingRoute extends _i9.PageRouteInfo<void> {
   const OnboardingRoute({List<_i9.PageRouteInfo>? children})
     : super(OnboardingRoute.name, initialChildren: children);
@@ -148,13 +102,13 @@ class OnboardingRoute extends _i9.PageRouteInfo<void> {
   static _i9.PageInfo page = _i9.PageInfo(
     name,
     builder: (data) {
-      return const _i6.OnboardingPage();
+      return const _i5.OnboardingPage();
     },
   );
 }
 
 /// generated route for
-/// [_i7.SignupPage]
+/// [_i6.SignupPage]
 class SignupRoute extends _i9.PageRouteInfo<void> {
   const SignupRoute({List<_i9.PageRouteInfo>? children})
     : super(SignupRoute.name, initialChildren: children);
@@ -164,13 +118,13 @@ class SignupRoute extends _i9.PageRouteInfo<void> {
   static _i9.PageInfo page = _i9.PageInfo(
     name,
     builder: (data) {
-      return const _i7.SignupPage();
+      return const _i6.SignupPage();
     },
   );
 }
 
 /// generated route for
-/// [_i8.SplashPage]
+/// [_i7.SplashPage]
 class SplashRoute extends _i9.PageRouteInfo<void> {
   const SplashRoute({List<_i9.PageRouteInfo>? children})
     : super(SplashRoute.name, initialChildren: children);
@@ -180,7 +134,56 @@ class SplashRoute extends _i9.PageRouteInfo<void> {
   static _i9.PageInfo page = _i9.PageInfo(
     name,
     builder: (data) {
-      return const _i8.SplashPage();
+      return const _i7.SplashPage();
     },
   );
+}
+
+/// generated route for
+/// [_i8.TabWrapper]
+class TabWrapper extends _i9.PageRouteInfo<TabWrapperArgs> {
+  TabWrapper({
+    _i10.Key? key,
+    String? address,
+    List<_i9.PageRouteInfo>? children,
+  }) : super(
+         TabWrapper.name,
+         args: TabWrapperArgs(key: key, address: address),
+         initialChildren: children,
+       );
+
+  static const String name = 'TabWrapper';
+
+  static _i9.PageInfo page = _i9.PageInfo(
+    name,
+    builder: (data) {
+      final args = data.argsAs<TabWrapperArgs>(
+        orElse: () => const TabWrapperArgs(),
+      );
+      return _i8.TabWrapper(key: args.key, address: args.address);
+    },
+  );
+}
+
+class TabWrapperArgs {
+  const TabWrapperArgs({this.key, this.address});
+
+  final _i10.Key? key;
+
+  final String? address;
+
+  @override
+  String toString() {
+    return 'TabWrapperArgs{key: $key, address: $address}';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    if (other is! TabWrapperArgs) return false;
+    return key == other.key && address == other.address;
+  }
+
+  @override
+  int get hashCode => key.hashCode ^ address.hashCode;
 }
