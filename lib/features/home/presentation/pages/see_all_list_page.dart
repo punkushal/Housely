03e33a885504333649d834fe1f07a5348ac98d5b
punkushal <spycymagar@gmail.com>
2025-12-27 14:12:47 +0545

    feat(ui): add divider to the list display

--- lib/features/home/presentation/pages/see_all_list_page.dart
+++ lib/features/home/presentation/pages/see_all_list_page.dart
@@ -1,5 +1,10 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:housely/core/constants/app_colors.dart';
+import 'package:housely/core/responsive/responsive_dimensions.dart';
+import 'package:housely/features/home/presentation/cubit/favorite_toggle_cubit.dart';
+import 'package:housely/features/home/presentation/widgets/small_card.dart';
 
 @RoutePage()
 class SeeAllListPage extends StatelessWidget {
@@ -8,6 +13,38 @@ class SeeAllListPage extends StatelessWidget {
   // later i also accept the list parameter to display data
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text(appBarTitle)));
+    return BlocProvider(
+      create: (context) => FavoriteToggleCubit(),
+      child: Scaffold(
+        appBar: AppBar(title: Text(appBarTitle)),
+        body: SafeArea(
+          child: ListView.builder(
+            itemCount: 6,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: ResponsiveDimensions.paddingSymmetric(
+                  context,
+                  horizontal: 24,
+                  vertical: 12,
+                ),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: Column(
+                    mainAxisSize: .min,
+                    spacing: ResponsiveDimensions.getHeight(context, 12),
+                    children: [
+                      SmallCard(
+                        height: ResponsiveDimensions.getHeight(context, 72),
+                      ),
+                      Divider(color: AppColors.divider),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
