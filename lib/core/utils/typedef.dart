0e49432ab4752cd64fa0bb3afe17e9127298dc66
punkushal <spycymagar@gmail.com>
2025-12-29 14:28:40 +0545

    chore: add stream in use case and type def

--- lib/core/utils/typedef.dart
+++ lib/core/utils/typedef.dart
@@ -3,3 +3,4 @@ import 'package:housely/core/error/failure.dart';
 
 typedef ResultFuture<T> = Future<Either<Failure, T>>;
 typedef ResultVoid = ResultFuture<void>;
+typedef ResultStream<T> = Stream<T>;
