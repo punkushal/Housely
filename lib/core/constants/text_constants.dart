c2482955d3ccadf0a9f78525614e44e398a298a5
punkushal <spycymagar@gmail.com>
2025-12-29 12:44:26 +0545

    feat(constants): add string constants

---
+++ lib/core/constants/text_constants.dart
@@ -0,0 +1,22 @@
+class TextConstants {
+  TextConstants._();
+
+  // bottom nav bar item labels
+  static final String home = "Home";
+  static final String explore = "Explore";
+  static final String add = "Add";
+  static final String booking = "My Booking";
+  static final String profile = "Profile";
+
+  // button labels
+  static final String signUp = "Sign up";
+  static final String login = "Log in";
+  static final String continueLabel = "Continue";
+  static final String currentLocation = "Use Current Location";
+  static final String manual = "Select it manually";
+  static final String started = "Get Started";
+  static final String next = "Next";
+  static final String save = "Save Change";
+  static final String rent = "Rent Now";
+  static final String submit = "Submit Review";
+}
