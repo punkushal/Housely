5de5a89c29270731118d301ea3da162fcee44244
punkushal <spycymagar@gmail.com>
2025-12-30 16:32:18 +0545

    feat(ui): add custom contact container widget

---
+++ lib/features/detail/presentation/widgets/contact_container.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:housely/core/constants/app_colors.dart';
+import 'package:housely/core/responsive/responsive_dimensions.dart';
+
+class ContactContainer extends StatelessWidget {
+  const ContactContainer({super.key, required this.iconPath});
+
+  ///svg icon path
+  final String iconPath;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(ResponsiveDimensions.getSize(context, 6)),
+      width: ResponsiveDimensions.getSize(context, 36),
+      height: ResponsiveDimensions.getHeight(context, 36),
+      decoration: BoxDecoration(shape: .circle, color: AppColors.chipInActive),
+      child: SvgPicture.asset(
+        iconPath,
+        width: ResponsiveDimensions.getSize(context, 24),
+        height: ResponsiveDimensions.getHeight(context, 24),
+        colorFilter: ColorFilter.mode(AppColors.primaryPressed, .srcIn),
+      ),
+    );
+  }
+}
