b6431857630c1b35c2240732e5d16e8973037626
punkushal <spycymagar@gmail.com>
2025-12-31 16:08:10 +0545

    checking conflict

--- lib/features/detail/presentation/pages/detail_page.dart
+++ lib/features/detail/presentation/pages/detail_page.dart
@@ -16,86 +16,80 @@ class DetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => FavoriteToggleCubit(),
-      child: Builder(
-        builder: (context) {
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Details'),
-              actionsPadding: ResponsiveDimensions.paddingOnly(
-                context,
-                left: 24,
-              ),
-              actions: [
-                // favorite icon button
-                GestureDetector(
-                  onTap: () {
-                    context.read<FavoriteToggleCubit>().toggleFavorite();
-                  },
-                  child:
-                      BlocSelector<
-                        FavoriteToggleCubit,
-                        FavoriteToggleState,
-                        bool
-                      >(
-                        selector: (state) {
-                          return state.isSelected;
-                        },
-                        builder: (context, state) {
-                          bool isFavorite = state;
-                          return SvgPicture.asset(
+    return Builder(
+      builder: (context) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Details'),
+            actionsPadding: ResponsiveDimensions.paddingOnly(context, left: 24),
+            actions: [
+              // favorite icon button
+              GestureDetector(
+                onTap: () {
+                  context.read<FavoriteToggleCubit>().toggleFavorite();
+                },
+                child:
+                    BlocSelector<
+                      FavoriteToggleCubit,
+                      FavoriteToggleState,
+                      bool
+                    >(
+                      selector: (state) {
+                        return state.isSelected;
+                      },
+                      builder: (context, state) {
+                        bool isFavorite = state;
+                        return SvgPicture.asset(
+                          isFavorite
+                              ? ImageConstant.favoriteFilledIcon
+                              : ImageConstant.favoriteIcon,
+                          width: ResponsiveDimensions.getSize(context, 24),
+                          height: ResponsiveDimensions.getHeight(context, 24),
+                          colorFilter: ColorFilter.mode(
                             isFavorite
-                                ? ImageConstant.favoriteFilledIcon
-                                : ImageConstant.favoriteIcon,
-                            width: ResponsiveDimensions.getSize(context, 24),
-                            height: ResponsiveDimensions.getHeight(context, 24),
-                            colorFilter: ColorFilter.mode(
-                              isFavorite
-                                  ? AppColors.error
-                                  : AppColors.textPrimary,
-                              .srcIn,
-                            ),
-                          );
-                        },
-                      ),
-                ),
-              ],
-            ),
-            body: SafeArea(
-              child: Padding(
-                padding: ResponsiveDimensions.paddingSymmetric(
-                  context,
-                  horizontal: 24,
-                ),
-                child: SingleChildScrollView(
-                  child: Column(
-                    spacing: ResponsiveDimensions.getHeight(context, 12),
-                    children: [
-                      // property image carousel
-                      CustomCarouselSlider(),
+                                ? AppColors.error
+                                : AppColors.textPrimary,
+                            .srcIn,
+                          ),
+                        );
+                      },
+                    ),
+              ),
+            ],
+          ),
+          body: SafeArea(
+            child: Padding(
+              padding: ResponsiveDimensions.paddingSymmetric(
+                context,
+                horizontal: 24,
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  spacing: ResponsiveDimensions.getHeight(context, 12),
+                  children: [
+                    // property image carousel
+                    CustomCarouselSlider(),
 
-                      // images list
-                      ImageList(),
+                    // images list
+                    ImageList(),
 
-                      SizedBox(
-                        height: ResponsiveDimensions.getHeight(context, 12),
-                      ),
+                    SizedBox(
+                      height: ResponsiveDimensions.getHeight(context, 12),
+                    ),
 
-                      // Detail section
-                      PropertyDetailSection(),
+                    // Detail section
+                    PropertyDetailSection(),
 
-                      SizedBox(
-                        height: ResponsiveDimensions.getHeight(context, 6),
-                      ),
-                    ],
-                  ),
+                    SizedBox(
+                      height: ResponsiveDimensions.getHeight(context, 6),
+                    ),
+                  ],
                 ),
               ),
             ),
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
   }
 }
