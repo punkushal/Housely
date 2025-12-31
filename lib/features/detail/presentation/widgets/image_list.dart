b2326e31d0815797d7dd9c0135f10e4a8e8af754
punkushal <spycymagar@gmail.com>
2025-12-31 15:18:33 +0545

    feat(ui): add image preview functionality

--- lib/features/detail/presentation/widgets/image_list.dart
+++ lib/features/detail/presentation/widgets/image_list.dart
@@ -6,23 +6,51 @@ import 'package:housely/features/detail/presentation/widgets/detail_image_card.d
 class ImageList extends StatelessWidget {
   const ImageList({super.key});
 
+  // handle image preview
+  void _previewImage(String imgPath, BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        contentPadding: .zero,
+        content: ClipRRect(
+          borderRadius: ResponsiveDimensions.borderRadiusMedium(context),
+          child: Image.asset(
+            imgPath,
+            height: ResponsiveDimensions.getSize(context, 188),
+            width: ResponsiveDimensions.getSize(context, 282),
+            fit: .cover,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: ResponsiveDimensions.getHeight(context, 72),
+      height: ResponsiveDimensions.getHeight(context, 86),
       child: ListView.builder(
         // TODO: later data will be dynamic
         scrollDirection: .horizontal,
         itemCount: 4,
         itemBuilder: (context, index) {
           return Padding(
-            padding: ResponsiveDimensions.paddingOnly(context, right: 8),
-            child: DetailImageCard(
-              imgPath: ImageConstant.secondVilla,
-              width: ResponsiveDimensions.getSize(context, 76),
-              height: ResponsiveDimensions.getHeight(context, 72),
-              fit: .cover,
-              radius: 8,
+            padding: ResponsiveDimensions.paddingOnly(
+              context,
+              // right: 8,
+              // bottom: 6,
+            ),
+            child: GestureDetector(
+              onTap: () {
+                _previewImage(ImageConstant.fourthVilla, context);
+              },
+              child: DetailImageCard(
+                imgPath: ImageConstant.fourthVilla,
+                width: ResponsiveDimensions.getSize(context, 76),
+                height: ResponsiveDimensions.getHeight(context, 72),
+                fit: .cover,
+                radius: 8,
+              ),
             ),
           );
         },
