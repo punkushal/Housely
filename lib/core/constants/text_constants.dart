43ad0407285583969b5e81cb1eebe87a5b52f40e
punkushal <spycymagar@gmail.com>
2026-01-02 17:14:22 +0545

    chore(constant): add new app text constant

--- lib/core/constants/text_constants.dart
+++ lib/core/constants/text_constants.dart
@@ -22,4 +22,11 @@ class TextConstants {
   static final String save = "Save Change";
   static final String rent = "Rent Now";
   static final String submit = "Submit Review";
+  static final String addProperty = "Add Property";
+
+  // error messages
+  static final String internetError =
+      "No internet connection. Please try again";
+  static final String uploadSingleFileError = "Please upload image";
+  static final String uploadManyFileError = "Please upload property images";
 }
