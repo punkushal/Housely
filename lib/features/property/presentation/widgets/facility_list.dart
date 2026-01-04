cf3ddd477a8dbd7862b432fa6e63231ea2e08d92
punkushal <spycymagar@gmail.com>
2026-01-04 09:45:28 +0545

    feat(ui): implement state and form validation

--- lib/features/property/presentation/widgets/facility_list.dart
+++ lib/features/property/presentation/widgets/facility_list.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:housely/core/constants/app_colors.dart';
 import 'package:housely/core/constants/app_text_style.dart';
 import 'package:housely/core/responsive/responsive_dimensions.dart';
+import 'package:housely/features/property/presentation/cubit/property_form_cubit.dart';
 
 class FacilityList extends StatefulWidget {
   const FacilityList({super.key});
@@ -11,59 +13,84 @@ class FacilityList extends StatefulWidget {
 }
 
 class _FacilityListState extends State<FacilityList> {
-  int _currentIndex = 0;
+  // The available options to display
+  final _facilityOptions = [
+    'Wifi',
+    'AC',
+    'parking',
+    'Gym',
+    'Gas station',
+    "Swimming Pool",
+  ];
+
+  // The list where we store what the user selects
+  final List<String> _selectedFacilites = [];
   @override
   Widget build(BuildContext context) {
-    final list = ['Wifi', 'AC', 'parking', 'Bathtubs', "Swimming Pool"];
-    return SizedBox(
-      height: ResponsiveDimensions.getSize(context, 36),
-      child: ListView.builder(
-        scrollDirection: .horizontal,
-        itemCount: list.length,
-        itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () {
-              setState(() {
-                _currentIndex = index;
-              });
-            },
-            child: FacilityChip(
-              label: list[index],
-              isActive: _currentIndex == index,
+    return FormField<List<String>>(
+      initialValue: _selectedFacilites,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please select at least one facility';
+        }
+        return null;
+      },
+      builder: (field) {
+        return Column(
+          crossAxisAlignment: .start,
+          mainAxisSize: .min,
+          children: [
+            Wrap(
+              spacing: 8,
+              runSpacing: 6,
+              children: _facilityOptions.map((facility) {
+                final isSelected = _selectedFacilites.contains(facility);
+
+                return FilterChip(
+                  label: Text(facility),
+                  selected: isSelected,
+                  checkmarkColor: AppColors.surface,
+                  selectedColor: AppColors.primary,
+                  backgroundColor: AppColors.chipInActive,
+                  labelStyle: AppTextStyle.bodyRegular(
+                    context,
+                    color: isSelected ? AppColors.surface : AppColors.textHint,
+                  ),
+                  onSelected: (selected) {
+                    setState(() {
+                      if (selected) {
+                        _selectedFacilites.add(facility);
+                      } else {
+                        _selectedFacilites.remove(facility);
+                      }
+                    });
+
+                    field.didChange(_selectedFacilites);
+
+                    // update form cubit to store the selected facilites
+                    context.read<PropertyFormCubit>().updateFacilities(
+                      _selectedFacilites,
+                    );
+                  },
+                );
+              }).toList(),
             ),
-          );
-        },
-      ),
-    );
-  }
-}
 
-// Facility chip
-class FacilityChip extends StatelessWidget {
-  const FacilityChip({super.key, required this.label, this.isActive = false});
-
-  /// label text
-  final String label;
-
-  /// is selected checker
-  final bool isActive;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: ResponsiveDimensions.paddingAll8(context),
-      margin: ResponsiveDimensions.paddingOnly(context, right: 8),
-      decoration: BoxDecoration(
-        border: Border.all(color: AppColors.border),
-        borderRadius: ResponsiveDimensions.borderRadiusSmall(context),
-        color: isActive ? AppColors.primary : AppColors.chipInActive,
-      ),
-      child: Text(
-        label,
-        style: AppTextStyle.bodyRegular(
-          context,
-          color: isActive ? AppColors.surface : AppColors.textHint,
-        ),
-      ),
+            // Error message display
+            if (field.hasError)
+              Padding(
+                padding: ResponsiveDimensions.paddingOnly(context, top: 8),
+                child: Text(
+                  field.errorText!,
+                  style: AppTextStyle.bodyRegular(
+                    context,
+                    color: AppColors.error,
+                  ),
+                ),
+              ),
+          ],
+        );
+      },
     );
   }
 }
