8a54f0f51e493598ed8602aa4872d9a7fa3b075c
punkushal <spycymagar@gmail.com>
2026-01-04 14:41:47 +0545

    feat(ui): add custom location card widget

---
+++ lib/features/property/presentation/widgets/location_card.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:housely/core/constants/image_constant.dart';
+import 'package:housely/core/responsive/responsive_dimensions.dart';
+import 'package:housely/core/widgets/custom_text_field.dart';
+import 'package:housely/features/property/presentation/cubit/property_form_cubit.dart';
+import 'package:housely/features/property/presentation/widgets/label.dart';
+
+class LocationCard extends StatelessWidget {
+  const LocationCard({super.key, this.address});
+  final String? address;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      spacing: ResponsiveDimensions.spacing4(context),
+      crossAxisAlignment: .start,
+      children: [
+        Label(label: "Location"),
+        BlocSelector<PropertyFormCubit, PropertyFormState, String?>(
+          selector: (state) {
+            return state.address;
+          },
+          builder: (context, address) {
+            if (address != null) {
+              return CustomTextField(initialValue: address);
+            }
+            return ClipRRect(
+              borderRadius: ResponsiveDimensions.borderRadiusMedium(context),
+              child: Image.asset(
+                ImageConstant.mapPreviewImg,
+                fit: .cover,
+                height: ResponsiveDimensions.getHeight(context, 142),
+                width: .infinity,
+              ),
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
