4fbc282be170083cdda88740828319aa217a4e88
punkushal <spycymagar@gmail.com>
2026-01-04 16:53:28 +0545

    feat(property): add use case for deleting  property image

---
+++ lib/features/property/domain/usecases/delete_image_file.dart
@@ -0,0 +1,27 @@
+import 'package:equatable/equatable.dart';
+import 'package:housely/core/usecases/usecase.dart';
+import 'package:housely/core/utils/typedef.dart';
+import 'package:housely/features/property/domain/repository/property_repo.dart';
+
+class DeleteImageFile implements UseCase<void, DeleteParam> {
+  final PropertyRepo repo;
+
+  DeleteImageFile({required this.repo});
+  @override
+  ResultFuture<void> call(params) async {
+    return await repo.deleteImageFile(
+      bucketId: params.bucketId,
+      fileId: params.fileId,
+    );
+  }
+}
+
+class DeleteParam extends Equatable {
+  final String bucketId;
+  final String fileId;
+
+  const DeleteParam({required this.bucketId, required this.fileId});
+
+  @override
+  List<Object?> get props => [bucketId, fileId];
+}
