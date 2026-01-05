7698b3ad53300049c33d446f858accc38b39b2ce
punkushal <spycymagar@gmail.com>
2026-01-05 16:31:45 +0545

    feat(owner): add abstract contrat for owner

---
+++ lib/features/property/domain/repository/owner_repo.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+import 'package:housely/core/utils/typedef.dart';
+import 'package:housely/features/property/domain/entities/property_owner.dart';
+
+abstract interface class OwnerRepo {
+  // upload property cover image
+  ResultFuture<Map<String, String>> uploadProfileImage({required File image});
+
+  // create owner profile
+  ResultVoid createOwnerProfile({required PropertyOwner owner});
+}
