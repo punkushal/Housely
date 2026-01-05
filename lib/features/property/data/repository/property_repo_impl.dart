da095c2bb25bc5b52638ff50e0fd8bda7deda01e
punkushal <spycymagar@gmail.com>
2026-01-05 10:08:35 +0545

    feat(property): implement abstract repository

---
+++ lib/features/property/data/repository/property_repo_impl.dart
@@ -0,0 +1,131 @@
+import 'dart:io';
+import 'package:appwrite/appwrite.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:housely/core/error/failure.dart';
+import 'package:housely/core/utils/typedef.dart';
+import 'package:housely/features/property/data/datasources/app_write_data_source.dart';
+import 'package:housely/features/property/domain/entities/property.dart';
+import 'package:housely/features/property/domain/repository/property_repo.dart';
+
+class PropertyRepoImpl implements PropertyRepo {
+  final AppwriteStorageDataSource dataSource;
+  final PropertyRepo repo;
+
+  PropertyRepoImpl({required this.dataSource, required this.repo});
+  @override
+  ResultVoid createProperty(Property property) async {
+    try {
+      await repo.createProperty(property);
+      return Right(null);
+    } on FirebaseAuthException catch (e) {
+      return Left(_handleFirebaseError(e));
+    } catch (e) {
+      return Left(ServerFailure("An unexpected failure occur: $e"));
+    }
+  }
+
+  @override
+  ResultVoid deleteImageFile({required String fileId}) async {
+    try {
+      await dataSource.deleteImageFile(fileId: fileId);
+      return Right(null);
+    } on AppwriteException catch (e) {
+      return Left(_handleError(e));
+    } catch (e) {
+      return Left(InvalidFileFailure("Failed to deleted image file: $e"));
+    }
+  }
+
+  @override
+  ResultFuture<Map<String, String>> updateImageFile({
+    required String fileId,
+  }) async {
+    try {
+      final result = await dataSource.updateImageFile(fileId: fileId);
+      return Right(result);
+    } on AppwriteException catch (e) {
+      return Left(_handleError(e));
+    } catch (e) {
+      return Left(InvalidFileFailure("Failed to update image file: $e"));
+    }
+  }
+
+  @override
+  ResultFuture<Map<String, String>> uploadCoverImage({
+    required File image,
+    required String ownerEmail,
+    required String folderType,
+  }) async {
+    try {
+      final result = await dataSource.uploadCoverImage(
+        image: image,
+        ownerEmail: ownerEmail,
+        folderType: folderType,
+      );
+      return Right(result);
+    } on AppwriteException catch (e) {
+      return Left(_handleError(e));
+    } catch (e) {
+      return Left(InvalidFileFailure("Failed to upload cover image: $e"));
+    }
+  }
+
+  @override
+  ResultFuture<Map<String, dynamic>> uploadPropertyImages({
+    required List<File> images,
+    required String ownerEmail,
+  }) async {
+    try {
+      final result = await dataSource.uploadPropertyImages(
+        images: images,
+        ownerEmail: ownerEmail,
+      );
+      return Right(result);
+    } on AppwriteException catch (e) {
+      return Left(_handleError(e));
+    } catch (e) {
+      return Left(InvalidFileFailure("Failed to upload cover image: $e"));
+    }
+  }
+
+  /// Centralized Error Handling
+  Failure _handleFirebaseError(FirebaseException e) {
+    switch (e.code) {
+      case 'permission-denied':
+        return PermissionFailure(
+          "You don't have permission to save this data. Check your Security Rules.",
+        );
+      case 'resource-exhausted':
+        return ResourceExceedFailure("Quota exceeded. Please try again later.");
+      case 'unavailable':
+        return NetworkFailure(
+          "The service is currently unavailable. Check your internet connection.",
+        );
+      case 'deadline-exceeded':
+        return DeadlineExceedFailure(
+          "The request took too long. Try a smaller data payload.",
+        );
+      default:
+        return UnknownFailure("Firestore Error [${e.code}]: ${e.message}");
+    }
+  }
+
+  Failure _handleError(AppwriteException e) {
+    // Appwrite uses numerical codes (404, 401, 429) and string types
+    switch (e.type) {
+      case 'user_unauthorized':
+        return UnauthorizedFailure(
+          "Error: Check your Bucket permissions in Appwrite Console.",
+        );
+
+      case 'storage_file_not_found':
+        return InvalidFileFailure(
+          "Error: The file you are trying to delete/access is gone.",
+        );
+
+      default:
+        return InvalidFileFailure("Appwrite Error (${e.code}): ${e.message}");
+    }
+  }
+}
