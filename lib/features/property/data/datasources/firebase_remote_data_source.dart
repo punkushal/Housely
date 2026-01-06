094c8dfe9075fd80b555855c5594727ced5a0751
punkushal <spycymagar@gmail.com>
2026-01-06 09:41:47 +0545

    feat(data): add functions for creating and fetching owner profile

--- lib/features/property/data/datasources/firebase_remote_data_source.dart
+++ lib/features/property/data/datasources/firebase_remote_data_source.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:housely/core/constants/text_constants.dart';
 import 'package:housely/features/property/data/models/property_model.dart';
+import 'package:housely/features/property/data/models/property_owner_model.dart';
 
 class FirebaseRemoteDataSource {
   final FirebaseFirestore firestore;
@@ -27,6 +28,33 @@ class FirebaseRemoteDataSource {
       return auth.currentUser!.email!;
     } catch (e) {
       throw Exception("Failed to get email: $e");
+    }
+  }
+
+  // Property owner
+  Future<void> createOwnerProfile(PropertyOwnerModel owner) async {
+    try {
+      final docId = auth.currentUser!.email;
+      await firestore
+          .collection(TextConstants.owners)
+          .doc(docId!)
+          .set(owner.toJson());
+    } catch (e) {
+      throw Exception('Failed to create owner profile: $e');
+    }
+  }
+
+  Future<PropertyOwnerModel?> getOwnerProfile() async {
+    try {
+      final docRef = firestore
+          .collection(TextConstants.owners)
+          .doc(auth.currentUser!.email);
+
+      final result = await docRef.get();
+      if (result.data() == null) return null;
+      return PropertyOwnerModel.fromJson(result.data()!);
+    } catch (e) {
+      throw Exception("Failed to fetch owner profile: $e");
     }
   }
 }
