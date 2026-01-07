19e18dd2e993bdf3f330d5fd991ca9e23c988f9e
punkushal <spycymagar@gmail.com>
2026-01-07 14:43:35 +0545

    refactor(ui): add property list parameter

--- lib/features/home/presentation/widgets/property_list.dart
+++ lib/features/home/presentation/widgets/property_list.dart
@@ -1,12 +1,23 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:housely/app/app_router.gr.dart';
 import 'package:housely/core/constants/app_colors.dart';
 import 'package:housely/core/responsive/responsive_dimensions.dart';
+import 'package:housely/features/home/presentation/cubit/favorite_toggle_cubit.dart';
 import 'package:housely/features/home/presentation/widgets/small_card.dart';
+import 'package:housely/features/property/domain/entities/property.dart';
 
 class PropertyList extends StatelessWidget {
-  const PropertyList({super.key, this.horizontal = 24, this.vertical = 12});
+  const PropertyList({
+    super.key,
+    this.horizontal = 24,
+    this.vertical = 12,
+    required this.propertyList,
+  });
+
+  /// property list
+  final List<Property> propertyList;
 
   /// horizontal padding
   final double horizontal;
@@ -15,32 +26,35 @@ class PropertyList extends StatelessWidget {
   final double vertical;
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      // TODO: later dynamice list item will be fetched
-      itemCount: 6,
-      itemBuilder: (context, index) {
-        return Padding(
-          padding: ResponsiveDimensions.paddingSymmetric(
-            context,
-            horizontal: horizontal,
-            vertical: vertical,
-          ),
-          child: SizedBox(
-            width: double.infinity,
-            child: Column(
-              mainAxisSize: .min,
-              spacing: ResponsiveDimensions.getSize(context, 12),
-              children: [
-                SmallCard(
-                  height: ResponsiveDimensions.getSize(context, 72),
-                  navigateTo: () => context.router.push(DetailRoute()),
-                ),
-                Divider(color: AppColors.divider),
-              ],
+    return BlocProvider(
+      create: (context) => FavoriteToggleCubit(),
+      child: ListView.builder(
+        itemCount: propertyList.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: ResponsiveDimensions.paddingSymmetric(
+              context,
+              horizontal: horizontal,
+              vertical: vertical,
             ),
-          ),
-        );
-      },
+            child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                mainAxisSize: .min,
+                spacing: ResponsiveDimensions.getSize(context, 12),
+                children: [
+                  SmallCard(
+                    property: propertyList[index],
+                    height: ResponsiveDimensions.getSize(context, 72),
+                    navigateTo: () => context.router.push(DetailRoute()),
+                  ),
+                  Divider(color: AppColors.divider),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
