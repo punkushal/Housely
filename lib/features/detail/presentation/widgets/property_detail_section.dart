78ec9258075fc95d90cd9d735299f97b7ab6dcc2
punkushal <spycymagar@gmail.com>
2026-01-08 14:47:09 +0545

    refactor(ui): add delete button with dialog box

--- lib/features/detail/presentation/widgets/property_detail_section.dart
+++ lib/features/detail/presentation/widgets/property_detail_section.dart
@@ -1,11 +1,13 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:housely/app/app_router.gr.dart';
 import 'package:housely/core/constants/app_colors.dart';
 import 'package:housely/core/constants/app_text_style.dart';
 import 'package:housely/core/constants/image_constant.dart';
 import 'package:housely/core/constants/text_constants.dart';
+import 'package:housely/core/extensions/string_extension.dart';
 import 'package:housely/core/responsive/responsive_dimensions.dart';
 import 'package:housely/core/widgets/custom_button.dart';
 import 'package:housely/features/detail/presentation/widgets/contact_container.dart';
@@ -16,14 +18,62 @@ import 'package:housely/features/detail/presentation/widgets/info_container.dart
 import 'package:housely/features/detail/presentation/widgets/read_more_text.dart';
 import 'package:housely/features/detail/presentation/widgets/review_list.dart';
 import 'package:housely/features/home/presentation/widgets/heading_section.dart';
+import 'package:housely/features/property/domain/entities/property.dart';
+import 'package:housely/features/property/presentation/cubit/owner_cubit.dart';
 
-// TODO: later value will be dynamic and access through variables
 class PropertyDetailSection extends StatelessWidget {
-  const PropertyDetailSection({super.key});
+  const PropertyDetailSection({super.key, required this.property});
+  final Property property;
+
+  // show dialog box before deleting
+  void _showConfirmationDailog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        title: Text("Delete Property?"),
+        content: Text(
+          "This action cannot be undone, and all related data will be permanently removed.",
+          style: AppTextStyle.bodyRegular(context),
+        ),
+
+        actions: [
+          Row(
+            mainAxisAlignment: .center,
+            children: [
+              TextButton(
+                onPressed: () {
+                  context.pop();
+                },
+                child: Text(
+                  'Cancel',
+                  style: AppTextStyle.bodySemiBold(context),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  //TODO: after edit i will do delete process
+                  //  context.read<PropertyBloc>().add(DeleteProperty(propertyId: ));
+                },
+                child: Text(
+                  'Delete',
+                  style: AppTextStyle.bodySemiBold(
+                    context,
+                    color: AppColors.error,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
-      spacing: ResponsiveDimensions.getHeight(context, 16),
+      spacing: ResponsiveDimensions.spacing16(context),
       mainAxisSize: .min,
       crossAxisAlignment: .start,
       children: [
@@ -38,7 +88,7 @@ class PropertyDetailSection extends StatelessWidget {
               children: [
                 // property title
                 Text(
-                  "House of Mormon",
+                  property.name.capitalize,
                   style: AppTextStyle.bodySemiBold(
                     context,
                     fontSize: 20,
@@ -58,7 +108,7 @@ class PropertyDetailSection extends StatelessWidget {
                     ),
 
                     Text(
-                      'Denspasar, Bali',
+                      property.location.address,
                       style: AppTextStyle.bodyRegular(
                         context,
                         fontSize: 14,
@@ -73,11 +123,10 @@ class PropertyDetailSection extends StatelessWidget {
             // price
             RichText(
               text: TextSpan(
-                text: "\$310",
+                text: "\$${property.price.amount.toStringAsFixed(2)}",
                 style: AppTextStyle.labelBold(
                   context,
                   lineHeight: 18,
-                  fontSize: 14,
                   color: AppColors.primaryPressed,
                 ),
                 children: [
@@ -86,6 +135,7 @@ class PropertyDetailSection extends StatelessWidget {
                     style: AppTextStyle.bodyRegular(
                       context,
                       lineHeight: 14,
+                      fontSize: 9,
                       color: AppColors.textHint,
                     ),
                   ),
@@ -109,17 +159,17 @@ class PropertyDetailSection extends StatelessWidget {
                 IconInfoContainer(
                   label: "Bedrooms",
                   iconPath: ImageConstant.bedIcon,
-                  number: 3,
+                  number: property.specs.bedrooms,
                 ),
                 IconInfoContainer(
                   label: "Bathtub",
                   iconPath: ImageConstant.bathTubIcon,
-                  number: 3,
+                  number: property.specs.bathrooms,
                 ),
                 IconInfoContainer(
                   label: "Bedrooms",
                   iconPath: ImageConstant.areaIcon,
-                  number: 1880,
+                  number: property.specs.area.toInt(),
                   isArea: true,
                 ),
               ],
@@ -128,12 +178,15 @@ class PropertyDetailSection extends StatelessWidget {
             Row(
               mainAxisAlignment: .spaceEvenly,
               children: [
-                InfoContainer(label: "Build", number: 2020),
+                InfoContainer(
+                  label: "Build",
+                  number: int.tryParse(property.specs.builtYear),
+                ),
                 Spacer(),
-                InfoContainer(label: "Parking", infoText: "1 Indoor"),
+                InfoContainer(label: "Parking", number: 1),
 
                 SizedBox(width: ResponsiveDimensions.getSize(context, 67)),
-                InfoContainer(label: "Status"),
+                InfoContainer(label: "Status", infoText: property.status.name),
 
                 SizedBox(width: ResponsiveDimensions.getSize(context, 21)),
               ],
@@ -145,8 +198,7 @@ class PropertyDetailSection extends StatelessWidget {
         // Description section
         HeadingLabel(label: "Description"),
         ReadMoreText(
-          text:
-              "This is a short description about this property. I hope you like this. It's surrounding is something that you might love it",
+          text: property.description,
           style: AppTextStyle.bodyRegular(context, color: AppColors.textHint),
         ),
 
@@ -157,17 +209,20 @@ class PropertyDetailSection extends StatelessWidget {
         Row(
           spacing: ResponsiveDimensions.spacing16(context),
           children: [
-            // TODO: later actual image will be placed
             CircleAvatar(
               radius: ResponsiveDimensions.radiusXLarge(context, size: 22),
               backgroundColor: Colors.grey,
+              foregroundImage: NetworkImage(property.media.coverImage['url']),
             ),
 
             Column(
               crossAxisAlignment: .start,
               children: [
                 // user name
-                Text("Kushal Pun", style: AppTextStyle.bodySemiBold(context)),
+                Text(
+                  property.owner.name.capitalize,
+                  style: AppTextStyle.bodySemiBold(context),
+                ),
 
                 // user role
                 Text(
@@ -196,12 +251,10 @@ class PropertyDetailSection extends StatelessWidget {
         // location & facility
         HeadingLabel(label: "Location & Public Facilities"),
 
-        FacilityList(),
+        FacilityList(facilities: property.facilities),
 
         // map preview image
         GestureDetector(
-          // TODO: later if agent or owner added their location then only this preview
-          // vissible otherwise hidden
           onTap: () => context.router.push(MapPickerRoute()),
           child: ClipRRect(
             borderRadius: ResponsiveDimensions.borderRadiusMedium(context),
@@ -214,13 +267,26 @@ class PropertyDetailSection extends StatelessWidget {
           ),
         ),
 
+        // TODO: initially this section will not be displayed untill review exist for this property
         // Review section
         HeadingSection(title: "Reviews", onTapText: "See all"),
 
         ReviewList(),
 
         // rent button
-        CustomButton(onTap: () {}, buttonLabel: TextConstants.rent),
+        BlocBuilder<OwnerCubit, OwnerState>(
+          builder: (context, state) {
+            if (state is OwnerLoaded && state.owner != null) {
+              return CustomButton(
+                onTap: () => _showConfirmationDailog(context),
+                buttonLabel: "Delete",
+                textColor: AppColors.error,
+                isOutlined: true,
+              );
+            }
+            return CustomButton(onTap: () {}, buttonLabel: TextConstants.rent);
+          },
+        ),
       ],
     );
   }
