45e1e31d3101c19e5f314b0130087c1858a35a64
punkushal <spycymagar@gmail.com>
2026-01-08 09:47:35 +0545

    refactor(ui): implement dynamic data values

--- lib/features/detail/presentation/widgets/facility_chip.dart
+++ lib/features/detail/presentation/widgets/facility_chip.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:housely/core/constants/app_colors.dart';
 import 'package:housely/core/constants/app_text_style.dart';
 import 'package:housely/core/responsive/responsive_dimensions.dart';
 
 class FacilityChip extends StatelessWidget {
-  const FacilityChip({super.key, required this.iconPath, required this.label});
-
-  /// svg icon path
-  final String iconPath;
+  const FacilityChip({super.key, required this.label});
 
   /// label
   final String label;
@@ -26,19 +22,7 @@ class FacilityChip extends StatelessWidget {
         borderRadius: ResponsiveDimensions.borderRadiusSmall(context),
         color: AppColors.chipInActive,
       ),
-      child: Row(
-        spacing: ResponsiveDimensions.spacing4(context),
-        crossAxisAlignment: .start,
-        children: [
-          SvgPicture.asset(
-            iconPath,
-            height: ResponsiveDimensions.getHeight(context, 18),
-            width: ResponsiveDimensions.getSize(context, 18),
-            colorFilter: ColorFilter.mode(AppColors.primaryPressed, .srcIn),
-          ),
-          Text(label, style: AppTextStyle.bodyMedium(context, fontSize: 12)),
-        ],
-      ),
+      child: Text(label, style: AppTextStyle.bodyMedium(context, fontSize: 12)),
     );
   }
 }
