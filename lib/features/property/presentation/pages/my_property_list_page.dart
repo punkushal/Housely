0eacf49e15fc0ed302d0c54875c215075e58796b
punkushal <spycymagar@gmail.com>
2026-01-09 09:47:11 +0545

    feat(ui): add snack message based on bloc state

--- lib/features/property/presentation/pages/my_property_list_page.dart
+++ lib/features/property/presentation/pages/my_property_list_page.dart
@@ -1,6 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:housely/core/constants/app_colors.dart';
+import 'package:housely/core/constants/app_text_style.dart';
 import 'package:housely/features/home/presentation/widgets/property_list.dart';
+import 'package:housely/features/property/presentation/bloc/property_bloc.dart';
 
 @RoutePage()
 class MyPropertyListPage extends StatefulWidget {
@@ -15,7 +19,24 @@ class _MyPropertyListPageState extends State<MyPropertyListPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("My properties")),
-      body: PropertyList(),
+      body: BlocBuilder<PropertyBloc, PropertyFetchState>(
+        builder: (context, state) {
+          if (state is PropertyLoaded && state.allProperties.isNotEmpty) {
+            return PropertyList(propertyList: state.allProperties);
+          } else if (state is PropertyFetchLoading) {
+            return Center(child: CircularProgressIndicator());
+          }
+          return Center(
+            child: Text(
+              "No properties added yet",
+              style: AppTextStyle.headingSemiBold(
+                context,
+                color: AppColors.border,
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
