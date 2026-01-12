b4b0c3bf6c7425088fdbe3e39218b6870ad7f56c
punkushal <spycymagar@gmail.com>
2026-01-12 16:15:29 +0545

    feat(state): provide search filter cubit

--- lib/features/search/presentation/page/explore_page.dart
+++ lib/features/search/presentation/page/explore_page.dart
@@ -1,9 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:housely/core/constants/image_constant.dart';
 import 'package:housely/core/responsive/responsive_dimensions.dart';
 import 'package:housely/core/widgets/custom_text_field.dart';
+import 'package:housely/features/search/presentation/cubit/search_filter_cubit.dart';
 import 'package:housely/features/search/presentation/widgets/filter_sheet.dart';
 
 @RoutePage()
@@ -23,54 +25,64 @@ class _ExplorePageState extends State<ExplorePage> {
     super.dispose();
   }
 
-  void showFitlerSheet() {
+  void showFitlerSheet(BuildContext context) {
+    // Capture the existing cubit instance from the current context
+    final searchFilterCubit = context.read<SearchFilterCubit>();
     showModalBottomSheet(
       useSafeArea: true,
       isScrollControlled: true,
       context: context,
-      builder: (context) => FilterSheet(),
+      builder: (context) =>
+          BlocProvider.value(value: searchFilterCubit, child: FilterSheet()),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Explore')),
-      body: SafeArea(
-        child: Padding(
-          padding: ResponsiveDimensions.paddingSymmetric(
-            context,
-            horizontal: 24,
-          ),
-          child: Column(
-            children: [
-              CustomTextField(
-                controller: _controller,
-                prefixIcon: SvgPicture.asset(
-                  ImageConstant.searchIcon,
-                  height: ResponsiveDimensions.getHeight(context, 24),
-                  width: ResponsiveDimensions.getSize(context, 24),
-                  fit: .scaleDown,
-                ),
-                suffixIcon: GestureDetector(
-                  onTap: () => showFitlerSheet(),
-                  child: SvgPicture.asset(
-                    ImageConstant.filterIcon,
-                    height: ResponsiveDimensions.getHeight(context, 24),
-                    width: ResponsiveDimensions.getSize(context, 24),
-                    fit: .scaleDown,
-                  ),
-                ),
-                hintText: "Search Property",
-                contentPadding: ResponsiveDimensions.paddingSymmetric(
+    return BlocProvider(
+      create: (context) => SearchFilterCubit(),
+      child: Builder(
+        builder: (context) {
+          return Scaffold(
+            appBar: AppBar(title: Text('Explore')),
+            body: SafeArea(
+              child: Padding(
+                padding: ResponsiveDimensions.paddingSymmetric(
                   context,
-                  horizontal: 16,
-                  vertical: 14,
+                  horizontal: 24,
+                ),
+                child: Column(
+                  children: [
+                    CustomTextField(
+                      controller: _controller,
+                      prefixIcon: SvgPicture.asset(
+                        ImageConstant.searchIcon,
+                        height: ResponsiveDimensions.getHeight(context, 24),
+                        width: ResponsiveDimensions.getSize(context, 24),
+                        fit: .scaleDown,
+                      ),
+                      suffixIcon: GestureDetector(
+                        onTap: () => showFitlerSheet(context),
+                        child: SvgPicture.asset(
+                          ImageConstant.filterIcon,
+                          height: ResponsiveDimensions.getHeight(context, 24),
+                          width: ResponsiveDimensions.getSize(context, 24),
+                          fit: .scaleDown,
+                        ),
+                      ),
+                      hintText: "Search Property",
+                      contentPadding: ResponsiveDimensions.paddingSymmetric(
+                        context,
+                        horizontal: 16,
+                        vertical: 14,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
