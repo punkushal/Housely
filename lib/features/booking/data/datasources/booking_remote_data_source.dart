0f968a2a72ffa77f1d5d903e81a9b02812d6c919
punkushal <spycymagar@gmail.com>
2026-01-16 14:37:01 +0545

    refactor(data): add tenant id and filter the list

--- lib/features/booking/data/datasources/booking_remote_data_source.dart
+++ lib/features/booking/data/datasources/booking_remote_data_source.dart
@@ -1,19 +1,27 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:housely/core/constants/text_constants.dart';
+import 'package:housely/features/auth/data/datasources/auth_remote_data_source.dart';
 import 'package:housely/features/booking/data/models/booking_model.dart';
 import 'package:housely/features/booking/domain/entity/booking.dart';
 
 class BookingRemoteDataSource {
   final FirebaseFirestore firestore;
-
-  BookingRemoteDataSource(this.firestore);
+  final AuthRemoteDataSource authRemoteDataSource;
+  BookingRemoteDataSource({
+    required this.authRemoteDataSource,
+    required this.firestore,
+  });
 
   // Request booking
   Future<void> requestBooking(Booking booking) async {
     try {
+      final currentUser = await authRemoteDataSource.getCurrentUser();
       final docRef = firestore.collection(TextConstants.bookings).doc();
 
-      final updatedBooking = booking.copyWith(bookingId: docRef.id);
+      final updatedBooking = booking.copyWith(
+        bookingId: docRef.id,
+        tenantId: currentUser!.uid,
+      );
 
       docRef.set(BookingModel.fromEntity(updatedBooking).toJson());
     } catch (e) {
@@ -37,7 +45,12 @@ class BookingRemoteDataSource {
 
   // Listen booking changes
   Stream<List<Booking>> listenBookingChanges() async* {
-    final snapshots = await firestore.collection(TextConstants.bookings).get();
+    final currentUser = await authRemoteDataSource.getCurrentUser();
+
+    final snapshots = await firestore
+        .collection(TextConstants.bookings)
+        .where('bookingId', isEqualTo: currentUser!.uid)
+        .get();
     final jsonList = snapshots.docs;
     final bookingList = jsonList
         .map((doc) => BookingModel.fromJson(doc.data()))
