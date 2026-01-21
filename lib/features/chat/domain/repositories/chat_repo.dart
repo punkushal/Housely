56f682f9b5d2683bcb4d4d834cede80baa837c00
punkushal <spycymagar@gmail.com>
2026-01-21 11:18:24 +0545

    feat(domain): add abstract methods for chatting

---
+++ lib/features/chat/domain/repositories/chat_repo.dart
@@ -0,0 +1,30 @@
+import 'package:housely/core/utils/typedef.dart';
+import 'package:housely/features/chat/domain/entity/chat_room.dart';
+import 'package:housely/features/chat/domain/entity/message.dart';
+
+abstract interface class ChatRepository {
+  // Stream to listen for the list of chats (Inbox)
+  ResultStream<List<ChatRoom>> getChatRooms();
+
+  // Stream to listen for messages in a specific room with pagination support
+  ResultStream<List<Message>> getMessages({
+    required String roomId,
+    int limit = 20,
+  });
+
+  // Method to fetch MORE messages (Pagination)
+  ResultFuture<List<Message>> getMoreMessages({
+    required String roomId,
+    required DateTime lastMessageTime,
+    int limit = 20,
+  });
+
+  // Action to send a message
+  ResultVoid sendMessage({
+    required ChatRoom chatRoom,
+    required Message message,
+  });
+
+  // Update online/offline status
+  ResultVoid updatePresence(bool isOnline);
+}
