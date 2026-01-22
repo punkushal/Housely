55337f3a4a1a31850d7ffd3f6dcb95b2579b61ae
punkushal <spycymagar@gmail.com>
2026-01-22 09:59:43 +0545

    refactor(model): add other params for more functionality

--- lib/features/chat/data/models/message_model.dart
+++ lib/features/chat/data/models/message_model.dart
@@ -3,55 +3,70 @@ import 'package:housely/features/chat/domain/entity/message.dart';
 
 class MessageModel extends Message {
   const MessageModel({
-    required super.id,
+    required super.messageId,
     required super.senderId,
     required super.receiverId,
-    required super.text,
+    required super.message,
     required super.timestamp,
+    required super.isDeleted,
+    required super.isRead,
+    required super.deletedBy,
   });
 
   factory MessageModel.fromFirestore(DocumentSnapshot doc) {
     final data = doc.data() as Map<String, dynamic>;
     return MessageModel(
-      id: doc.id,
+      messageId: doc.id,
       senderId: data['senderId'],
       receiverId: data['receiverId'],
-      text: data['text'],
+      message: data['message'],
       timestamp: (data['timestamp'] as Timestamp).toDate(),
+      isRead: data['isRead'] ?? false,
+      isDeleted: data['isDeleted'] ?? false,
+      deletedBy: List<String>.from(data['deletedBy'] ?? []),
     );
   }
 
   factory MessageModel.fromEntity(Message message) {
     return MessageModel(
-      id: message.id,
+      messageId: message.messageId,
       senderId: message.senderId,
       receiverId: message.receiverId,
-      text: message.text,
+      message: message.message,
       timestamp: message.timestamp,
+      isDeleted: message.isDeleted,
+      isRead: message.isRead,
+      deletedBy: message.deletedBy,
     );
   }
 
   Map<String, dynamic> toJson() => {
     'senderId': senderId,
     'receiverId': receiverId,
-    'text': text,
+    'text': message,
     'timestamp': FieldValue.serverTimestamp(),
   };
 
   @override
   MessageModel copyWith({
-    String? id,
+    String? messageId,
     String? senderId,
     String? receiverId,
-    String? text,
+    String? message,
     DateTime? timestamp,
+    bool? isRead,
+    bool? isDeleted,
+    List<String>? deletedBy,
   }) {
     return MessageModel(
-      id: id ?? this.id,
+      messageId: messageId ?? this.messageId,
       senderId: senderId ?? this.senderId,
       receiverId: receiverId ?? this.receiverId,
-      text: text ?? this.text,
+      message: message ?? this.message,
       timestamp: timestamp ?? this.timestamp,
+      isDeleted: isDeleted ?? this.isDeleted,
+      isRead: isRead ?? this.isRead,
+      deletedBy: deletedBy ?? this.deletedBy,
     );
   }
 }
