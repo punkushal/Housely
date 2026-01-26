2b0ba237ccf10b47483ee6407537a9fd684406f0
punkushal <spycymagar@gmail.com>
2026-01-26 15:23:53 +0545

    feat(domain): add entity class for review

---
+++ lib/features/review/domain/entity/review.dart
@@ -0,0 +1,34 @@
+import 'package:equatable/equatable.dart';
+
+class Review extends Equatable {
+  final String reviewId;
+  final String userId;
+  final String userName;
+  final String? userProfile;
+  final double rating;
+  final String comment;
+  final DateTime createdAt;
+  final DateTime? updatedAt;
+
+  const Review({
+    required this.reviewId,
+    required this.userId,
+    required this.userName,
+    this.userProfile,
+    required this.rating,
+    required this.comment,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+  @override
+  List<Object?> get props => [
+    reviewId,
+    userId,
+    userName,
+    userProfile,
+    rating,
+    comment,
+    createdAt,
+    updatedAt,
+  ];
+}
