a595c9f8f09d6c4e0da3f834000fef74487aef9f
punkushal <spycymagar@gmail.com>
2026-01-28 11:33:20 +0545

    feat(data): implement abstract methods

---
+++ lib/features/review/data/repository/review_repo_impl.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+
+import 'package:appwrite/appwrite.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:housely/core/error/failure.dart';
+import 'package:housely/core/utils/handle_error.dart';
+import 'package:housely/core/utils/typedef.dart';
+import 'package:housely/features/review/data/datasource/review_remote_data_source.dart';
+import 'package:housely/features/review/domain/entity/review.dart';
+import 'package:housely/features/review/domain/repository/review_repo.dart';
+
+class ReviewRepoImpl implements ReviewRepo {
+  final ReviewRemoteDataSource remoteDataSource;
+
+  ReviewRepoImpl(this.remoteDataSource);
+  @override
+  ResultVoid addReview({
+    required Review review,
+    required String propertyId,
+  }) async {
+    try {
+      await remoteDataSource.addReview(propertyId: propertyId, review: review);
+      return Right(null);
+    } on FirebaseException catch (e) {
+      return Left(handleFirebaseError(e));
+    } catch (e) {
+      return Left(ServerFailure("Failed to add new review :$e"));
+    }
+  }
+
+  @override
+  ResultFuture<Map<String, dynamic>> uploadReviewImages({
+    required String userEmail,
+    required List<File> images,
+  }) async {
+    try {
+      final result = await remoteDataSource.uploadReviewImages(
+        images: images,
+        userEmail: userEmail,
+      );
+
+      return Right(result);
+    } on AppwriteException catch (e) {
+      return Left(handleAppWriteError(e));
+    } catch (e) {
+      return Left(ServerFailure("Failed to upload review images :$e"));
+    }
+  }
+}
