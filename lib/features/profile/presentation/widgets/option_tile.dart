0f311778b7eefd77157989880135615e051b434c
punkushal <spycymagar@gmail.com>
2026-01-30 13:59:20 +0545

    refactor(ui): remove default padding and add text style

--- lib/features/profile/presentation/widgets/option_tile.dart
+++ lib/features/profile/presentation/widgets/option_tile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:housely/core/constants/app_colors.dart';
+import 'package:housely/core/constants/app_text_style.dart';
 
 class OptionTile extends StatelessWidget {
   const OptionTile({
@@ -15,12 +16,13 @@ class OptionTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      contentPadding: .zero,
       onTap: onTap,
       leading: SvgPicture.asset(
         iconPath,
         colorFilter: ColorFilter.mode(AppColors.primaryPressed, .srcIn),
       ),
-      title: Text(label),
+      title: Text(label, style: AppTextStyle.bodyMedium(context, fontSize: 12)),
       trailing: Icon(Icons.arrow_forward_ios, color: AppColors.divider),
     );
   }
