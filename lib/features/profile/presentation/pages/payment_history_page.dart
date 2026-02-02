be73e4eb44a995a0d5e5610c15b43d297f691a15
punkushal <spycymagar@gmail.com>
2026-02-02 08:16:14 +0545

    refactor(ui): implement payment history cubit

--- lib/features/profile/presentation/pages/payment_history_page.dart
+++ lib/features/profile/presentation/pages/payment_history_page.dart
@@ -1,5 +1,12 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:housely/core/constants/app_colors.dart';
+import 'package:housely/core/utils/snack_bar_helper.dart';
+import 'package:housely/features/auth/presentation/cubit/auth_cubit.dart';
+import 'package:housely/features/profile/presentation/cubit/payments/cubit/payment_history_cubit.dart';
+import 'package:housely/features/profile/presentation/widgets/payment_history_card.dart';
+import 'package:housely/injection_container.dart';
 
 @RoutePage()
 class PaymentHistoryPage extends StatelessWidget {
@@ -7,6 +14,47 @@ class PaymentHistoryPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Payment History")));
+    final authState = context.read<AuthCubit>().state as Authenticated;
+    return BlocProvider(
+      create: (context) =>
+          sl<PaymentHistoryCubit>()
+            ..getAllPaymentHistoryList(tenantId: authState.currentUser!.uid),
+      child: Builder(
+        builder: (context) {
+          return BlocListener<PaymentHistoryCubit, PaymentHistoryState>(
+            listener: (context, state) {
+              if (state is PaymentHistoryFailure) {
+                return SnackbarHelper.showError(context, state.message);
+              }
+            },
+            child: Scaffold(
+              backgroundColor: AppColors.surface.withValues(alpha: 0.95),
+              appBar: AppBar(title: Text("Payment History")),
+              body: BlocBuilder<PaymentHistoryCubit, PaymentHistoryState>(
+                builder: (context, state) {
+                  if (state is PaymentHistoryLoading) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (state is PaymentHistoryLoaded) {
+                    if (state.allPaymentHistoryList.isEmpty) {
+                      return Center(child: Text('No payment history'));
+                    }
+                    return ListView.builder(
+                      itemCount: state.allPaymentHistoryList.length,
+                      itemBuilder: (context, index) {
+                        return PaymentHistoryCard(
+                          property: state.allPaymentHistoryList[index].property,
+                          booking: state.allPaymentHistoryList[index].booking,
+                        );
+                      },
+                    );
+                  }
+                  return SizedBox.shrink();
+                },
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
