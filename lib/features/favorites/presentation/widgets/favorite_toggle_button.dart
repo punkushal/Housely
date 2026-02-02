5b0ee2b831e237774d0a248d40235a57c2c7b934
punkushal <spycymagar@gmail.com>
2026-02-02 22:59:29 +0545

    refactor(ui): add toggle method

--- lib/features/favorites/presentation/widgets/favorite_toggle_button.dart
+++ lib/features/favorites/presentation/widgets/favorite_toggle_button.dart
@@ -5,23 +5,24 @@ import 'package:flutter_svg/svg.dart';
 import '../../../../core/constants/app_colors.dart';
 import '../../../../core/constants/image_constant.dart';
 import '../../../../core/responsive/responsive_dimensions.dart';
+import '../../../../core/utils/snack_bar_helper.dart';
+import '../../../property/domain/entities/property.dart';
+import '../../domain/entity/favorite.dart';
 import '../bloc/favorites_bloc.dart';
 
 class FavoriteToggleButton extends StatelessWidget {
-  const FavoriteToggleButton({
-    super.key,
-    required this.propertyId,
-    required this.favoriteToggle,
-  });
-  final String propertyId;
-  final void Function()? favoriteToggle;
+  const FavoriteToggleButton({super.key, required this.property});
+  final Property property;
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: favoriteToggle,
+      onTap: () {
+        _toggleFavorite(context, property);
+      },
       child: BlocBuilder<FavoritesBloc, FavoritesState>(
         builder: (context, state) {
-          bool isFav = _isFavorited(state, propertyId);
+          bool isFav = _isFavorited(state, property.id);
 
           return SvgPicture.asset(
             isFav
@@ -60,5 +61,29 @@ class FavoriteToggleButton extends StatelessWidget {
       // Default cases (initial, loading, error)
       _ => false,
     };
+  }
+
+  /// Toggle favorite status for a property
+  void _toggleFavorite(BuildContext context, Property property) {
+    // Ensure property has an ID
+    if (property.id == null) {
+      // Show error if property doesn't have an ID
+      SnackbarHelper.showError(
+        context,
+        "Cannot favorite: Property ID is missing",
+      );
+      return;
+    }
+
+    // Create favorite entity
+    final favorite = Favorite(
+      property: property,
+      favoriteId: property.id!,
+      addedAt: .now(),
+    );
+
+    context.read<FavoritesBloc>().add(
+      ToggleFavoriteRequested(favorite: favorite, favoriteId: property.id!),
+    );
   }
 }
