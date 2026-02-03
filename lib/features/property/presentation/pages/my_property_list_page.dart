217b46754fb0d45a4c73456ee57c3ee2a4bcd14f
punkushal <spycymagar@gmail.com>
2026-02-03 20:57:45 +0545

    refactor(ui): implement fetching owner property list with error handling

--- lib/features/property/presentation/pages/my_property_list_page.dart
+++ lib/features/property/presentation/pages/my_property_list_page.dart
@@ -3,17 +3,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:housely/core/constants/app_colors.dart';
 import 'package:housely/core/constants/app_text_style.dart';
+import 'package:housely/features/auth/presentation/cubit/auth_cubit.dart';
 import 'package:housely/features/home/presentation/widgets/property_list.dart';
-import 'package:housely/features/property/presentation/bloc/property_bloc.dart';
+import 'package:housely/features/property/presentation/bloc/fetch/property_list_bloc.dart';
+import 'package:housely/injection_container.dart';
 
+import '../../../../core/widgets/custom_button.dart';
 import '../../../favorites/presentation/bloc/favorites_bloc.dart';
 
 @RoutePage()
-class MyPropertyListPage extends StatefulWidget {
+class MyPropertyListPage extends StatefulWidget implements AutoRouteWrapper {
   const MyPropertyListPage({super.key});
 
   @override
   State<MyPropertyListPage> createState() => _MyPropertyListPageState();
+
+  @override
+  Widget wrappedRoute(BuildContext context) {
+    final authState = context.read<AuthCubit>().state as Authenticated;
+
+    return BlocProvider(
+      create: (context) =>
+          sl<PropertyListBloc>()
+            ..add(GetMyProperties(userId: authState.currentUser!.uid)),
+      child: this,
+    );
+  }
 }
 
 class _MyPropertyListPageState extends State<MyPropertyListPage> {
@@ -22,7 +37,6 @@ class _MyPropertyListPageState extends State<MyPropertyListPage> {
     super.initState();
 
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      context.read<PropertyBloc>().add(GetAllProperties());
       context.read<FavoritesBloc>().add(LoadFavoritesRequested());
     });
   }
@@ -30,13 +44,18 @@ class _MyPropertyListPageState extends State<MyPropertyListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("My properties")),
-      body: BlocBuilder<PropertyBloc, PropertyFetchState>(
+      appBar: AppBar(title: const Text("My properties")),
+      body: BlocBuilder<PropertyListBloc, PropertyListState>(
         builder: (context, state) {
-          if (state is PropertyLoaded && state.allProperties.isNotEmpty) {
+          if (state is PropertyListLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (state is PropertyListLoaded && state.allProperties.isNotEmpty) {
             return PropertyList(propertyList: state.allProperties);
-          } else if (state is PropertyFetchLoading) {
-            return Center(child: CircularProgressIndicator());
+          }
+
+          if (state is PropertyListFailure) {
+            return _buildErrorState(state.message);
           }
           return Center(
             child: Text(
@@ -48,6 +67,31 @@ class _MyPropertyListPageState extends State<MyPropertyListPage> {
             ),
           );
         },
+      ),
+    );
+  }
+
+  Widget _buildErrorState(String? message) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(message ?? 'Failed to load property'),
+          const SizedBox(height: 16),
+          CustomButton(
+            onTap: () {
+              // Retry loading
+              context.read<PropertyListBloc>().add(
+                GetMyProperties(
+                  userId: (context.read<AuthCubit>().state as Authenticated)
+                      .currentUser!
+                      .uid,
+                ),
+              );
+            },
+            buttonLabel: 'Retry',
+          ),
+        ],
       ),
     );
   }
