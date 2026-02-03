ee25b7169e3492ab1c88667290174fb6fc43b567
punkushal <spycymagar@gmail.com>
2026-02-03 20:53:48 +0545

    refactor(ui): modify ui to display property list and booking request section by default

--- lib/features/profile/presentation/pages/profile_page.dart
+++ lib/features/profile/presentation/pages/profile_page.dart
@@ -130,30 +130,19 @@ class _ProfilePageState extends State<ProfilePage> {
                         },
                       ),
 
-                      BlocBuilder<OwnerCubit, OwnerState>(
-                        builder: (context, state) {
-                          if (state is OwnerLoaded && state.owner != null) {
-                            return Column(
-                              children: [
-                                OptionTile(
-                                  label: "My properties list",
-                                  iconPath: ImageConstant.hospitalIcon,
-                                  onTap: () {
-                                    context.router.push(MyPropertyListRoute());
-                                  },
-                                ),
+                      OptionTile(
+                        label: "My properties list",
+                        iconPath: ImageConstant.hospitalIcon,
+                        onTap: () {
+                          context.router.push(MyPropertyListRoute());
+                        },
+                      ),
 
-                                OptionTile(
-                                  label: "Booking request",
-                                  iconPath: ImageConstant.infoSquareIcon,
-                                  onTap: () {
-                                    context.router.push(BookingRequestRoute());
-                                  },
-                                ),
-                              ],
-                            );
-                          }
-                          return SizedBox.shrink();
+                      OptionTile(
+                        label: "Booking request",
+                        iconPath: ImageConstant.infoSquareIcon,
+                        onTap: () {
+                          context.router.push(BookingRequestRoute());
                         },
                       ),
                     ],
