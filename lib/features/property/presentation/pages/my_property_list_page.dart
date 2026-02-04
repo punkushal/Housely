26b37246baff87ddd4c87797f45e732c02909fa0
punkushal <spycymagar@gmail.com>
2026-02-04 09:33:38 +0545

    refactor(ui): handle error state

--- lib/features/property/presentation/pages/my_property_list_page.dart
+++ lib/features/property/presentation/pages/my_property_list_page.dart
@@ -3,12 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:housely/core/constants/app_colors.dart';
 import 'package:housely/core/constants/app_text_style.dart';
+import 'package:housely/core/widgets/handle_error_state.dart';
 import 'package:housely/features/auth/presentation/cubit/auth_cubit.dart';
 import 'package:housely/features/home/presentation/widgets/property_list.dart';
 import 'package:housely/features/property/presentation/bloc/fetch/property_list_bloc.dart';
 import 'package:housely/injection_container.dart';
 
-import '../../../../core/widgets/custom_button.dart';
 import '../../../favorites/presentation/bloc/favorites_bloc.dart';
 
 @RoutePage()
@@ -47,15 +47,39 @@ class _MyPropertyListPageState extends State<MyPropertyListPage> {
       appBar: AppBar(title: const Text("My properties")),
       body: BlocBuilder<PropertyListBloc, PropertyListState>(
         builder: (context, state) {
-          if (state is PropertyListLoading) {
+          if (state is PropertyListLoading && state.section == .all) {
             return const Center(child: CircularProgressIndicator());
           }
-          if (state is PropertyListLoaded && state.allProperties.isNotEmpty) {
-            return PropertyList(propertyList: state.allProperties);
+          if (state is PropertyListLoaded) {
+            final properties = state.allProperties;
+
+            if (properties == null) {
+              return Center(
+                child: Text(
+                  "No properties added yet",
+                  style: AppTextStyle.headingSemiBold(
+                    context,
+                    color: AppColors.border,
+                  ),
+                ),
+              );
+            }
+            return PropertyList(propertyList: properties);
           }
 
-          if (state is PropertyListFailure) {
-            return _buildErrorState(state.message);
+          if (state is PropertyListFailure && state.section == .all) {
+            return HandleErrorState(
+              message: state.message,
+              retry: () {
+                context.read<PropertyListBloc>().add(
+                  GetMyProperties(
+                    userId: (context.read<AuthCubit>().state as Authenticated)
+                        .currentUser!
+                        .uid,
+                  ),
+                );
+              },
+            );
           }
           return Center(
             child: Text(
@@ -67,31 +91,6 @@ class _MyPropertyListPageState extends State<MyPropertyListPage> {
             ),
           );
         },
-      ),
-    );
-  }
-
-  Widget _buildErrorState(String? message) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(message ?? 'Failed to load property'),
-          const SizedBox(height: 16),
-          CustomButton(
-            onTap: () {
-              // Retry loading
-              context.read<PropertyListBloc>().add(
-                GetMyProperties(
-                  userId: (context.read<AuthCubit>().state as Authenticated)
-                      .currentUser!
-                      .uid,
-                ),
-              );
-            },
-            buttonLabel: 'Retry',
-          ),
-        ],
       ),
     );
   }
